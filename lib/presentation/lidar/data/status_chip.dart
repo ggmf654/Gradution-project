b5601fee315f275480fd66df590b19f18a7830c8
firas mouit <firasmoit814@gmail.com>
2026-03-28 13:51:31 +0300

    v2.2

--- lib/presentation/lidar/data/status_chip.dart
+++ lib/presentation/lidar/data/status_chip.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 Widget statusChip(String status, Color fallback) {
--- lib/presentation/radio/components/mission_row.dart
+++ lib/presentation/radio/components/mission_row.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'details_column.dart';
 import 'editable_detail_column.dart';
